0bae560330e1d2268738568468888abc8e227a32
Luis Flores <lf016158@gmail.com>
2021-03-25 00:49:43 -0600

    se creo la interfaz del Login  y se configuro el Evento para arrastrar el formulario y Salir

--- Proyecto Ferreteira - 1/Login.xaml.cs
+++ Proyecto Ferreteira - 1/Login.xaml.cs
@@ -24,5 +24,45 @@ namespace Proyecto_Ferreteira___1
         {
             InitializeComponent();
         }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+            //Usuarios user = new Usuarios();
+            //var validLogin = user.login_user(txtuser.Text, txtpass.Password);
+            //if (validLogin == true && txtuser.Text == Cache_Usuario.Usuario && txtpass.Password == Cache_Usuario.Contra)
+            //{
+            //    MessageBox.Show(string.Format("Bienvenido al sistema! {0},{1}", Cache_Usuario.Nombre, Cache_Usuario.Apellido));
+            //    Menu_Principal menu_Principal = new Menu_Principal();
+            //    menu_Principal.Show();
+            //    this.Close();
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Usuario Incorrecto o Contraseña\nIntente de nuevo", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+            //}
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Esta seguro que desea Salir?", "Advertencia", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                this.Close();
+            }
+        }
     }
 }
