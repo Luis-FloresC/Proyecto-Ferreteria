7d8b43dca192037319aed4f29c6cb0e59a69b258
Luis Flores <lf016158@gmail.com>
2021-03-25 00:57:27 -0600

    Creacion de la Carpeta Clases y la Clase Connection se Descargo del Adminsitrador de Paquetes Nuget: ConfigurationManager.

---
+++ Proyecto Ferreteira - 1/Clases/Connection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Proyecto_Ferreteira___1.Clases
+{
+    class Connection
+    {
+
+        private readonly String Connection_st;
+
+        public Connection()
+        {
+            Connection_st = ConfigurationManager.ConnectionStrings["System_Lf.Properties.Settings.SystemLfDb"].ConnectionString;
+
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(Connection_st);
+        }
+
+    }
+}
