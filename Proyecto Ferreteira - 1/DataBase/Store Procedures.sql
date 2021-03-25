64809a28e4fbe5f1acab6406a144341491110867
Luis Flores <lf016158@gmail.com>
2021-03-25 00:22:06 -0600

    se creo el archivo Store Procedure,que contiene el proceso alamcenado de Verificar Usuarios

---
+++ Proyecto Ferreteira - 1/DataBase/Store Procedures.sql
@@ -0,0 +1,21 @@
+
+--- Proceso Almacenado para verificar los Uusuarios
+create procedure Verificar_Usuario
+(
+@user nvarchar(50),
+@contrasenia nvarchar(50)
+)
+as begin
+
+Select E.Codigo_Empleado [Id],
+U.Nick_Name [Usuario],
+U.Contrasenia,
+CONCAT(E.Nombre_Empleado,' ',E.Apellido_empleado)[Nombre del Empleado],
+P.Descripcion [Cargo],
+E.Correo [Email]
+from [Recursos_humanos].[Empleado] E
+join [Recursos_humanos].[Puesto] P on P.Codigo_Puesto = E.Codigo_Puesto
+join [Recursos_humanos].[Usuario] U on U.Codigo_Empleado = E.Codigo_Empleado
+where U.Nick_Name = @user and U.Contrasenia = @contrasenia
+
+end
