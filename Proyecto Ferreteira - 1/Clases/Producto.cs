940faf05e9da5c6e5d94dacc27f4ef6e3fda69f7
Nayeli Ardon <nayeliardon19@gmail.com>
2021-04-03 16:49:16 -0600

    Creación de la clase productos que contendra los distintos métodos del módulo productos.

---
+++ Proyecto Ferreteira - 1/Clases/Producto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Namespace Requerido
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Proyecto_Ferreteira___1.Clases
+{
+    class Producto : Connection
+    {
+        //Propiedades
+        public int CodigoDeLaCategoria { get; set; }
+        public string NombreDeLaCategoria { get; set; }
+        public string NombreDelProducto { get; set; }
+        public int CantidadDeProducto { get; set; }
+        public int PrecioDelProducto { get; set; }
+
+        //Constructor
+        public Producto() { }
+
+
+        /// <summary>
+        /// Obtiene la lista de categorias de la base de datos
+        /// </summary>
+        /// <returns>Lista con los nombres de las categorias</returns>
+        public List<String> Categoria()
+        {
+            List<String> Categoria = new List<string>();
+            var connection = GetConnection();
+
+            try
+            {
+                string query = @"Select * Productos.Categoria.Codigo_Categoria, Productos.Producto.Nombre_Producto 
+                                From Productos.Categoria INNER JOIN Productos.Producto 
+                                ON Productos.Categoria.Codigo_Categoria = Productos.Producto.Codigo_Categoria"; //Consulta SQL
+
+                //Creacion de la consulta
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+
+                //Obtencion de datos 
+                using(SqlDataReader reader = command.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        Categoria.Add(NombreDeLaCategoria = reader.GetString(1));
+                    }
+                }
+                return Categoria;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+    }
+}
