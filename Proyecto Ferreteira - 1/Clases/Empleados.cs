a6d646aeae0cf904ee93d06bdac829a5895a1292
Luis Flores <lf016158@gmail.com>
2021-04-09 02:34:24 -0600

    se crearon metodos que retornan el resultado de la consulta de datos

---
+++ Proyecto Ferreteira - 1/Clases/Empleados.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Ferreteira___1.Clases
+{
+  public class Empleados
+    {
+        public Empleados() { }
+        UserData UserData = new UserData();
+
+        public List<UserData> ListaTotalEmpleados()
+        {
+            try
+            {
+                return UserData.DataGridEmpleados();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public List<UserData> Cargos()
+        {
+            try
+            {
+                return UserData.ComboBoxCargo();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+
+        public string AñadirNuevoEmpleado(string nombre, string apellido, int codigoCargo, string telefono, string correo, string direccion, bool estado, string fechaNac)
+        {
+            try
+            {
+                return UserData.RegistrarEmpleados(nombre,apellido,codigoCargo,telefono,correo,direccion,estado,fechaNac);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+
+    }
+}
