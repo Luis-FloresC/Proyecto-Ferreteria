cac41187efe2438f3b3d65b913308edb367b40c8
Alejandro <alejandromartinezxp@gmail.com>
2021-04-11 22:09:33 -0600

    Creacion de la clase ClsCliente

---
+++ Proyecto Ferreteira - 1/Clases/ClsCliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Proyecto_Ferreteira___1.Clases
+{
+    class ClsCliente
+    {
+
+
+        //Atributos
+        public int codigo { get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public string identidad { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+        public string telefono { get; set; }
+        public string rtn { get; set; }
+        public bool estado { get; set; }
+
+        public void AgregarCliente()
+        {
+
+        }
+    }
+}
