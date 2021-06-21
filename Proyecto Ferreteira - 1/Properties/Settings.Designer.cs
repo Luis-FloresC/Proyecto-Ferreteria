69c76967b9909073fae58bc740b2a3d9bd70175e
Ericka Castellanos <65922114+ErickaCastellanos@users.noreply.github.com>
2021-06-21 08:15:23 -0600

    Cambio conexión

--- Proyecto Ferreteira - 1/Properties/Settings.Designer.cs
+++ Proyecto Ferreteira - 1/Properties/Settings.Designer.cs
@@ -26,7 +26,8 @@ namespace Proyecto_Ferreteira___1.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=Ferreteria;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(local)\\SQLEXPRESS;Initial Catalog=Ferreteria;Integrated Security=Tru" +
+            "e")]
         public string FerreteriaDb {
             get {
                 return ((string)(this["FerreteriaDb"]));
