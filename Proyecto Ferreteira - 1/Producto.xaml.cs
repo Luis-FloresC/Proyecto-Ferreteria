ed43cee5ee52c0b0347407c42303261db33fe3f6
Nayeli Ardon <nayeliardon19@gmail.com>
2021-07-28 23:27:12 -0600

    Validaciones al momento de realizar la modificación de un producto.

--- Proyecto Ferreteira - 1/Producto.xaml.cs
+++ Proyecto Ferreteira - 1/Producto.xaml.cs
@@ -2,19 +2,10 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Text.RegularExpressions;
 
 
 namespace Proyecto_Ferreteira___1
@@ -91,7 +82,7 @@ namespace Proyecto_Ferreteira___1
             txtPrecioProducto.Text = String.Empty;
             txtExistenciaProducto.Text = String.Empty;
             cbNombreCategoria.SelectedValue = null;
-   
+
         }
 
         /// <summary>
@@ -125,10 +116,10 @@ namespace Proyecto_Ferreteira___1
                     }
                     else if (CadenaSoloEspacios(nombre2))
                     {
-                        MessageBox.Show("No se puede ingresar el nombre de un producto con espacios", 
+                        MessageBox.Show("No se puede ingresar el nombre de un producto con espacios",
                             "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
                     }
-                   
+
                     else
                     {
                         foreach (int i in IdCategoria)
@@ -146,13 +137,13 @@ namespace Proyecto_Ferreteira___1
                         LimpiarPantalla();
                     }
 
-                    
+
                 }
 
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message.ToString(),"Error");
+                MessageBox.Show(ex.Message.ToString(), "Error");
                 MessageBox.Show("Por favor verifique que esta ingresando los valores correctos en los " +
                     "campos", "Aviso", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
@@ -174,21 +165,42 @@ namespace Proyecto_Ferreteira___1
                 }
                 else
                 {
-                    foreach (int i in IdCategoria)
+                    string nombre2 = txtNombreProducto.Text;
+                    double precio2 = Convert.ToDouble(txtPrecioProducto.Text);
+
+                    if (nombre2.Length <= 1)
                     {
-                        if (cbNombreCategoria.SelectedIndex == c)
-                        {
-                            string nombre = txtNombreProducto.Text;
-                            double precio = Convert.ToDouble(txtPrecioProducto.Text);
-                            int productos = IdProducto;
-                            int existencia = Convert.ToInt32(txtExistenciaProducto.Text);
-                            producto.ModificarProductos(nombre, precio, i, productos, existencia);
-                        }
-                        c++;
+                        MessageBox.Show("El nombre no puede ser menor o igual a un caracter", "Advertencia",
+                            MessageBoxButton.OK, MessageBoxImage.Warning);
                     }
-                    MessageBox.Show("Se ha actualizado correctamente el producto", "Felicidades", MessageBoxButton.OK, MessageBoxImage.Information);
-                    MostrarDatos();
-                    LimpiarPantalla();
+                    else if (precio2 == 0)
+                    {
+                        MessageBox.Show("El precio no puede ser 0", "Advertencia",
+                            MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                    else if (CadenaSoloEspacios(nombre2))
+                    {
+                        MessageBox.Show("No se puede ingresar el nombre de un producto con espacios",
+                            "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                    else
+                    {
+                        foreach (int i in IdCategoria)
+                        {
+                            if (cbNombreCategoria.SelectedIndex == c)
+                            {
+                                string nombre = txtNombreProducto.Text;
+                                double precio = Convert.ToDouble(txtPrecioProducto.Text);
+                                int productos = IdProducto;
+                                int existencia = Convert.ToInt32(txtExistenciaProducto.Text);
+                                producto.ModificarProductos(nombre, precio, i, productos, existencia);
+                            }
+                            c++;
+                        }
+                        MessageBox.Show("Se ha actualizado correctamente el producto", "Felicidades", MessageBoxButton.OK, MessageBoxImage.Information);
+                        MostrarDatos();
+                        LimpiarPantalla();
+                    }
                 }
             }
             catch (Exception)
@@ -267,18 +279,15 @@ namespace Proyecto_Ferreteira___1
 
             if (source.Trim().Length <= 1)
             {
-
-
                 return true;
             }
             else
             {
-
                 return false;
             }
 
         }
 
-      
+
     }
 }
